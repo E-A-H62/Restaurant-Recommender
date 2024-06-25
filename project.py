343d89f6657449a10f5b452ad06efca7aef1bbd6
Bryson <bryseturner0130@gmail.com>
2024-06-25 16:44:05 -0500

    Style changes

--- project.py
+++ project.py
@@ -5,21 +5,22 @@ from openai import OpenAI
 
 # Retrieve the API key from the environment variable
 my_api_key = os.getenv('OPENAI_KEY')
-# TEST
-# print(my_api_key)
-
 
 class Project:
 
     @staticmethod
-    def store_db(data, db_url='sqlite:///data_base_name.db', table_name='table_name'):
+    def store_db(data, db_url='sqlite:///data_base_name.db', 
+                 table_name='table_name'):
         df = pd.DataFrame.from_dict(data)
         engine = db.create_engine(db_url)
         
-        df.to_sql(table_name, con=engine, if_exists='replace', index=False)
+        df.to_sql(table_name, con=engine, 
+                  if_exists='replace', index=False)
         
         with engine.connect() as connection:
-            query_result = connection.execute(db.text(f"SELECT * FROM {table_name};")).fetchall()
+            query_result = connection.execute(
+            db.text("SELECT * FROM table_name;")
+            ).fetchall()
             return pd.DataFrame(query_result)
     
     def generate_chat(api_key):
@@ -30,8 +31,11 @@ class Project:
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are well informed about popular restaurants."},
-                {"role": "user", "content": "Generate a table with five popular restaurants. The data should include an id, name, and number ratings."}
+            {"role": "system", "content": "You can create organized datatables."},
+            {"role": "user", "content": (
+            "Generate a table with 10 items. The data contains name, age, birthday, "
+            "and nationality."
+            )}
             ]
         )
         

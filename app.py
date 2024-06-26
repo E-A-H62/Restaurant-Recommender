b3848969a4414dd320fac6268a29ba331ca19813
Bryson <bryseturner0130@gmail.com>
2024-06-26 15:30:49 -0500

    Fixing all display

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
-
+from flask import Flask, request, render_template
 from project import Project
 
 app = Flask(__name__)
 
 my_api_key = os.getenv('OPENAI_KEY')
-
 
 @app.route("/")
 def home():
@@ -17,9 +15,7 @@ def submit_data():
     age = request.form["age"]
     cuisine = request.form["cuisine"]
     df = Project.get_recs(my_api_key, age, cuisine)
-    return df.to_html()
-
-
+    return render_template("results.html", tables=[df.to_html(classes='table table-striped', index=False)], titles=df.columns.values)
 
 if __name__ == '__main__':
     app.run(debug=True)

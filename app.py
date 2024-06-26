ed3ff5fe86dd036c7d1ac4577c7626a619519226
E-A-H62 <eahernandez1212@gmail.com>
2024-06-26 13:06:56 -0700

    Fixed formatting in api call and styling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 
 from project import Project
 
@@ -12,13 +12,13 @@ my_api_key = os.getenv('OPENAI_KEY')
 def home():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
 def submit_data():
     age = request.form["age"]
     cuisine = request.form["cuisine"]
     df = Project.get_recs(my_api_key, age, cuisine)
     return df.to_html()
-
 
 
 if __name__ == '__main__':
